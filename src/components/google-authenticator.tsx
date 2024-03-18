3b81eecc8453dab2452b99c9b1c3a24a69cca8be
Eduard Ramirez <edd.ramirez@proton.me>
2024-03-18 13:39:52 -0500

    Refactor GoogleAuthenticator component

--- src/components/google-authenticator.tsx
+++ src/components/google-authenticator.tsx
@@ -2,6 +2,7 @@ import { FirebaseError } from "firebase/app";
 import { GoogleAuthProvider } from "firebase/auth";
 
 import {
+  QRL,
   Slot,
   component$,
   useContext,
@@ -17,108 +18,131 @@ import { AuthService } from "../services/auth.service";
 
 import "./google-authenticator.css";
 
-interface Props {}
+interface Props {
+  onSignIn$?: QRL<(user: UserModel) => void>;
+  onSignOut$?: QRL<() => void>;
+  onError$?: QRL<(error: FirebaseError) => void>;
+}
 
-export const GoogleAuthenticator = component$<Props>(() => {
-  const firebaseConfig = useContext(FirebaseConfigContext);
-  const userSigned = useSignal<UserModel>();
-  const profileButtonList = useSignal<boolean>();
+export const GoogleAuthenticator = component$<Props>(
+  ({ onSignIn$, onSignOut$, onError$ }) => {
+    const firebaseConfig = useContext(FirebaseConfigContext);
+    const userSigned = useSignal<UserModel>();
+    const profileButtonList = useSignal<boolean>();
 
-  // eslint-disable-next-line qwik/no-use-visible-task
-  useVisibleTask$(async () => {
-    const currentUser = await new AuthService(
-      JSON.parse(CRYPTER.decrypt(firebaseConfig))
-    ).currentUser();
+    // eslint-disable-next-line qwik/no-use-visible-task
+    useVisibleTask$(async () => {
+      const currentUser = await new AuthService(
+        JSON.parse(CRYPTER.decrypt(firebaseConfig))
+      ).currentUser();
 
-    if (!currentUser || currentUser instanceof FirebaseError)
-      return console.error("Error getting current user");
+      if (!currentUser || currentUser instanceof FirebaseError)
+        return console.error("Error getting current user");
 
-    userSigned.value = {
-      disabled: false,
-      email: currentUser?.email,
-      displayName: currentUser?.displayName,
-      photoURL: currentUser?.photoURL,
-      uid: currentUser?.uid,
-      emailVerified: currentUser?.emailVerified,
-      phoneNumber: currentUser?.phoneNumber,
-      provider: currentUser?.providerId as UserModel["provider"],
-    };
-  });
+      userSigned.value = {
+        disabled: false,
+        email: currentUser?.email,
+        displayName: currentUser?.displayName,
+        photoURL: currentUser?.photoURL,
+        uid: currentUser?.uid,
+        emailVerified: currentUser?.emailVerified,
+        phoneNumber: currentUser?.phoneNumber,
+        provider: currentUser?.providerId as UserModel["provider"],
+      };
+    });
 
-  return (
-    <>
-      {userSigned.value ? (
-        <div class="btn-list">
-          <button
-            class="btn btn-profile-signed"
-            onClick$={() =>
-              (profileButtonList.value = !profileButtonList.value)
-            }
-          >
-            <span>
-              <strong>{userSigned.value.displayName}</strong>
-              <small>{userSigned.value.email}</small>
-            </span>
-            {userSigned.value.photoURL && (
-              <img
-                src={userSigned.value.photoURL}
-                alt={`Foto de perfil de ${userSigned.value.displayName}`}
-                width={70}
-                height={70}
-              />
-            )}
-            <ChevronDownIcon color="#000000" width="20" height="20" />
-          </button>
-          {profileButtonList.value && (
+    return (
+      <>
+        {userSigned.value ? (
+          <div class="btn-list">
             <button
-              class="btn-sign-out"
-              onClick$={async () => {
-                try {
-                  await new AuthService(
-                    JSON.parse(CRYPTER.decrypt(firebaseConfig))
-                  ).signOut();
-                  userSigned.value = undefined;
-                } catch (error) {
-                  console.error("Error signing out", error);
-                }
-              }}
-            >
-              Salir
-            </button>
-          )}
-        </div>
-      ) : (
-        <button
-          class="btn btn-signin-google"
-          onClick$={async () => {
-            try {
-              const user = await new AuthService(
-                JSON.parse(CRYPTER.decrypt(firebaseConfig))
-              ).signInWithPopup(new GoogleAuthProvider());
-
-              if (user instanceof FirebaseError) {
-                console.error("Error signing in with Google", user);
-                return;
+              class="btn btn-profile-signed"
+              onClick$={() =>
+                (profileButtonList.value = !profileButtonList.value)
               }
+            >
+              <span>
+                <strong>{userSigned.value.displayName}</strong>
+                <small>{userSigned.value.email}</small>
+              </span>
+              {userSigned.value.photoURL && (
+                <img
+                  src={userSigned.value.photoURL}
+                  alt={`Foto de perfil de ${userSigned.value.displayName}`}
+                  width={70}
+                  height={70}
+                />
+              )}
+              <ChevronDownIcon color="#000000" width="20" height="20" />
+            </button>
+            {profileButtonList.value && (
+              <button
+                class="btn-sign-out"
+                onClick$={async () => {
+                  try {
+                    await new AuthService(
+                      JSON.parse(CRYPTER.decrypt(firebaseConfig))
+                    ).signOut();
+                    userSigned.value = undefined;
+                    onSignOut$ && onSignOut$();
+                  } catch (error: any) {
+                    onError$ &&
+                      onError$({
+                        code: error.code,
+                        message: error.message,
+                        name: "SIGN_OUT_ERROR",
+                      });
 
-              userSigned.value = {
-                disabled: false,
-                email: user?.email,
-                displayName: user?.displayName,
-                photoURL: user?.photoURL,
-                uid: user?.uid,
-                emailVerified: user?.emailVerified,
-                phoneNumber: user?.phoneNumber,
-                provider: user?.providerId as UserModel["provider"],
-              };
-            } catch (error) {
-              console.error("Error signing in with Google", error);
-            }
-          }}
-        >
-          <Slot />
-        </button>
-      )}
-    </>
-  );
-});
+                    console.error("signing out error", error);
+                  }
+                }}
+              >
+                Salir
+              </button>
+            )}
+          </div>
+        ) : (
+          <button
+            class="btn btn-signin-google"
+            onClick$={async () => {
+              try {
+                const user = await new AuthService(
+                  JSON.parse(CRYPTER.decrypt(firebaseConfig))
+                ).signInWithPopup(new GoogleAuthProvider());
+
+                if (user instanceof FirebaseError) {
+                  console.error("Error signing in with Google", user);
+                  return;
+                }
+
+                userSigned.value = {
+                  disabled: false,
+                  email: user?.email,
+                  displayName: user?.displayName,
+                  photoURL: user?.photoURL,
+                  uid: user?.uid,
+                  emailVerified: user?.emailVerified,
+                  phoneNumber: user?.phoneNumber,
+                  provider: user?.providerId as UserModel["provider"],
+                };
+
+                onSignIn$ && onSignIn$(userSigned.value);
+              } catch (error: any) {
+                onError$ &&
+                  onError$({
+                    code: error.code,
+                    message: error.message,
+                    name: "GOOGLE_SIGNIN_POPUP_ERROR",
+                  });
+
+                console.error("Google sign in popup error", error);
+              }
+            }}
+          >
+            <Slot />
+          </button>
+        )}
+      </>
+    );
+  }
+);
