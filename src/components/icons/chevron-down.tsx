94b8b7ce7fd92f79d5b0d1bdd49a9e8afda329dd
Eduard Ramirez <edd.ramirez@proton.me>
2024-03-18 13:24:04 -0500

    Add BrandGoogleFilledIcon and update ChevronDownIcon

--- src/components/icons/chevron-down.tsx
+++ src/components/icons/chevron-down.tsx
@@ -1,13 +1,18 @@
-export const ChevronIcon = () => {
+interface Props {
+  color: string;
+  height?: string;
+  width?: string;
+}
+export const ChevronDownIcon = ({ color, height, width }: Props) => {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
       class="icon icon-tabler icon-tabler-chevron-down"
-      width="44"
-      height="44"
+      width={width || "44"}
+      height={height || "44"}
       viewBox="0 0 24 24"
       stroke-width="1.5"
-      stroke="#2c3e50"
+      stroke={color || "#2c3e50"}
       fill="none"
       stroke-linecap="round"
       stroke-linejoin="round"
