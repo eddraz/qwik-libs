ac8fc36ebbf3c93d3812f9ad63efc3d1593d642e
Eduard Ramirez <edd.ramirez@proton.me>
2024-03-18 19:12:00 -0500

    Add new exports for crypter, auth service, and firebase service

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,5 @@
 export { GoogleAuthenticator } from "./components/auth/google-authenticator";
 export { Auth } from "./components/auth/auth";
+export { CRYPTER } from "./utils/crypter.util";
+export { AuthService } from "./services/auth.service";
+export { FirebaseService } from "./services/firebase.service";
