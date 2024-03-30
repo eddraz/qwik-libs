54f8de90799e656d22835f8d5dbde08db75225c2
edd.ramirez@proton.me <edd.ramirez@proton.me>
2024-03-29 21:43:38 -0500

    Refactor UserModel interface

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -1,12 +1,12 @@
 export interface UserModel {
-  uid: string;
-  displayName: string | null;
-  email: string | null;
-  photoURL: string | null;
-  emailVerified: boolean;
-  phoneNumber: string | null;
-  disabled: boolean;
-  provider:
+  uid?: string;
+  displayName?: string | null;
+  email?: string | null;
+  photoURL?: string | null;
+  emailVerified?: boolean;
+  phoneNumber?: string | null;
+  disabled?: boolean;
+  provider?:
     | "gql"
     | "google"
     | "facebook"
