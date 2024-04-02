ebbfcfe41aa4a83e7f7ff48a69b90bd6f0067010
edd.ramirez@proton.me <edd.ramirez@proton.me>
2024-04-02 15:04:19 -0500

    Add pay component to instance

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 export { GoogleAuthenticator } from "./components/auth/google-authenticator";
 export { Auth } from "./components/auth/auth";
+export { Pay } from "./components/pay/pay";
 export { CRYPTER } from "./utils/crypter.util";
 export { AuthService } from "./services/auth.service";
 export { FirebaseService } from "./services/firebase.service";
