62478640c8bbe9b0de8aed37df1469db045e4838
edd.ramirez@proton.me <edd.ramirez@proton.me>
2024-04-03 19:36:29 -0500

    Add instances of auth components

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,9 @@
-export { GoogleAuthenticator } from "./components/auth/google-authenticator";
 export { Auth } from "./components/auth/auth";
+export { SignUpForm } from "./components/auth/sign-up-form";
+export { SigninForm } from "./components/auth/signin-form";
+export { ForgotPasswordForm } from "./components/auth/forgot-password-form";
+export { GoogleAuthenticator } from "./components/auth/google-authenticator";
+export { UserProfile } from "./components/auth/user-profile";
 export { Pay } from "./components/pay/pay";
 export { CRYPTER } from "./utils/crypter.util";
 export { AuthService } from "./services/auth.service";
